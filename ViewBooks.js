4f45ebb5196158a5efa5e036636f1508ba784244
PONRAJ M <123803809+PonrajMarikannan@users.noreply.github.com>
2023-06-04 15:57:53 +0530

    Add files via upload

---
+++ ViewBooks.js
@@ -0,0 +1,88 @@
+const display=document.getElementById("displayBook");
+const apiURL="https://www.googleapis.com/books/v1/volumes?q=search+terms";
+const max=10;
+let startIndex=0;
+const createElement=(tag)=> document.createElement(tag);
+
+const createBookElement=(Id,titles,authors,image,publishers,publishdates)=> 
+{
+    const div=createElement("div");
+    div.setAttribute("id", Id);
+    div.classList.add("book-card"); 
+    display.appendChild(div);
+
+    const img=createElement("img");
+    img.classList.add("image");
+    img.src=image;
+    div.appendChild(img);
+
+    const title=createElement("h3");
+    title.classList.add("title");
+    title.innerText=`Title: ${titles}`;
+    div.appendChild(title);
+
+    const author=createElement("h3");
+    author.classList.add("author");
+    author.innerText=`Author: ${authors}`;
+    div.appendChild(author);
+
+    const publisher=createElement("h3");
+    publisher.classList.add("publisher");
+    publisher.innerText=`Publisher: ${publishers}`;
+    div.appendChild(publisher);
+
+    const publishdate=createElement("h3");
+    publishdate.classList.add("publishdate");
+    publishdate.innerText=`Publish Date: ${publishdates}`;
+    div.appendChild(publishdate);
+};
+const fetchBooks=()=>
+ {
+  const bk=new XMLHttpRequest();
+  bk.onload=()=> 
+  {
+    if (bk.status==200) 
+    {
+      const response=JSON.parse(bk.responseText);
+      const books=response.items;
+      console.log(books);
+      const bookData=[];
+      for(let book of books) 
+      {
+        const id=book.id;
+        const title=book.volumeInfo.title;
+        const authors=book.volumeInfo.authors;
+        const thumbnail=book.volumeInfo.imageLinks?.thumbnail;
+        const publisher=book.volumeInfo.publisher;
+        const publisherdate=book.volumeInfo.publishedDate;
+        if (id&&title&&authors&&thumbnail&&publisher&&publisherdate)
+        {
+          bookData.push({id,title,authors,thumbnail,publisher,publisherdate});
+        }
+      }
+      displayBooks(bookData);
+      if(startIndex+max<response.totalItems) 
+      {
+        startIndex=startIndex+max;
+        fetchBooks();
+      }
+    } 
+    else 
+    {
+      console.log("Error:",bk.status);
+    }
+  };
+  const url=`${apiURL}&startIndex=${startIndex}&max=${max}`;
+  bk.open("GET", url);
+  bk.send();
+};
+const displayBooks=(bookData)=> 
+{
+  for(let book of bookData) 
+  {
+    const{id,title,authors,thumbnail,publisher,publisherdate}=book;
+    createBookElement(id,title,authors,thumbnail,publisher,publisherdate);
+  }
+};
+
+fetchBooks();
